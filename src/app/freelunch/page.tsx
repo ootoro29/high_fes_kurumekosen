434f5357b2fd7b008fb69e0b6e74937aa397bd49
ootoro29 <gobou256@gmail.com>
2024-02-20 22:53:20 +0900

    ゲームコンテンツ説明追加

--- src/app/freelunch/page.tsx
+++ src/app/freelunch/page.tsx
@@ -1,8 +1,23 @@
+import Image from "next/image"
+
 export default function STG(){
     return(
         <>
-           <h1 className="w-full text-3xl font-bold">フリーランチ</h1>
-           <iframe className="aspect-video w-full max-w-[640px]" width="560" height="315" src="https://www.youtube.com/embed/G4_Ck977UBk?si=aJ9Vi0U9eEMXUuNK" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"></iframe>
+           <div className="py-2">
+                <h1 className="w-full text-4xl font-bold p-1 -indent-1">フリーランチ</h1>
+                <p className="indent-5">ジャンル:3Dシューティングゲーム</p>
+                <p className="indent-5">左右から流れてくるマトを、撃って撃って撃ちまくりましょう！壊した数に応じてスコアが変動します。目標スコアは1万6000点！</p>
+            </div>
+
+            <div>
+                <p className="font-bold">↓プレイ中の画面↓</p>
+                <Image className=" py-2 w-auto max-w-[640px]" width = {640} height={360} src = {`/game2-op.jpg`} alt = "" priority={true} />
+            </div>
+
+            <div className="py-2">
+                <h1 className="w-full text-2xl font-bold indent-3">猫ミーム</h1>
+                <iframe className="aspect-video w-full max-w-[640px]" width="560" height="315" src="https://www.youtube.com/embed/G4_Ck977UBk?si=aJ9Vi0U9eEMXUuNK" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"></iframe>
+            </div>
         </>
     );
 }
--- src/app/magudial/page.tsx
+++ src/app/magudial/page.tsx
@@ -1,7 +1,37 @@
+import Image from "next/image";
 export default function STG(){
     return(
         <>
-           <h1 className="w-full text-3xl font-bold">MAGUDIAL</h1>
+            <div className="py-2">
+                <h1 className="w-full text-4xl font-bold p-1 -indent-1">MAGUDIAL</h1>
+                <p className="indent-5">ジャンル:2D全方位シューティングゲーム</p>
+                <p className="indent-5">敵の弾幕をうまく掻い潜りこちらの攻撃を当てていく、スピード感のあるゲームになっています。</p>
+            </div>
+            <div>
+                <p className="font-bold">↓タイトル画面↓</p>
+                <Image className=" py-2 w-auto max-w-[640px]" width = {640} height={360} src = {`/game1.png`} alt = "" priority={true} />
+            </div>
+            <div>
+                <p className="font-bold">↓プレイ中の画面↓</p>
+                <Image className=" py-2 w-auto max-w-[640px]" width = {640} height={360} src = {`/game1-op.png`} alt = "" priority={true} />
+            </div>
+            <div className="py-2">
+                <h1 className="w-full text-2xl font-bold indent-3">操作方法</h1>
+                <p className="indent-5">Wキーで上方向に移動</p>
+                <p className="indent-5">Aキーで左方向に移動</p>
+                <p className="indent-5">Sキーで下方向に移動</p>
+                <p className="indent-5">Dキーで右方向に移動</p>
+                <p className="indent-5">スペースキーでダッシュ回避</p>
+                <p className="indent-8 text-gray-500">水色のリングは回避のクールタイムを表す。</p>
+                <p className="indent-8 text-gray-500">自機周りの黄色い円は無敵時間を表す。</p>
+                <p className="indent-5">マウスの左部分を長押しして攻撃</p>
+                <p className="indent-8 text-gray-500">攻撃の方向は自機からマウスポインタへの方向</p>
+                <p className="indent-5">マウスの右部分を長押しで低速移動</p>
+            </div>
+            <div className="py-2">
+                <h1 className="w-full text-2xl font-bold indent-3">クリア動画</h1>
+                <iframe className="aspect-video w-full max-w-[640px]" width="560" height="315" src="https://www.youtube.com/embed/OMKHXi2cDds?si=bL1jFOxGU1nWIXt1" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"></iframe>
+            </div>
         </>
     );
 }
